20fc3a0b0af4e243658211dd8dee460bc38b057f
Quintin Dwight <qhdwight@gmail.com>
2019-01-06 17:27:06 -0800

    Update, added CMAKE and basic driving

---
+++ cmake-build-debug/CMakeFiles/fake_frcUserProgram.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for fake_frcUserProgram.
+# This may be replaced when dependencies are built.
--- src/main/cpp/robot.cpp
+++ src/main/cpp/robot.cpp
@@ -5,16 +5,22 @@
 class Robot : public frc::IterativeRobot {
 public:
   void TeleopInit() override {
-    
+    m_LeftSPX.Follow(m_LeftSRX);
+    m_RightSPX.Follow(m_RightSRX);
   }
 
   void TeleopPeriodic() override {
+    double
+      forward = -m_Stick.GetY(),
+      turn = m_Stick.GetX();
+    m_LeftSRX.Set(ControlMode::PercentOutput, forward + turn*2);
+    m_RightSRX.Set(ControlMode::PercentOutput, forward + turn*2);
   }
 
 private:
   frc::Joystick m_Stick { 0 };
-  TalonSRX m_LeftSRX { 0 }, m_RightSRX { 1 };
-  VictorSPX m_LeftSPX { 2 }, m_RightSPX { 3 };
+  TalonSRX m_RightSRX { 0 }, m_LeftSRX { 1 };
+  VictorSPX m_RightSPX { 2 }, m_LeftSPX { 3 };
 };
 
 #ifndef RUNNING_FRC_TESTS

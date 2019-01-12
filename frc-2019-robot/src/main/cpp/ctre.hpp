5a1182b25a8d6e5aad2856f646ce60c5eee95ab4
BuildTools <qhdwight@gmail.com>
2019-01-12 10:25:38 -0800

    Think CLion project is working now

---
+++ frc-2019-robot/src/main/cpp/ctre.hpp
@@ -0,0 +1,14 @@
+#include <ctre/phoenix/motorcontrol/can/TalonSRX.h>
+#include <ctre/phoenix/motorcontrol/can/VictorSPX.h>
+
+namespace ctr {
+    using namespace ctre;
+    using namespace ctre::phoenix;
+    using namespace ctre::phoenix::motion;
+    using namespace ctre::phoenix::motorcontrol;
+    using namespace ctre::phoenix::motorcontrol::can;
+//    using namespace ctre::phoenix::sensors;
+//    using namespace ctre::phoenix::signals;
+//    using namespace ctre::phoenix::tasking;
+//    using namespace ctre::phoenix::tasking::schedulers;
+}
--- frc-2019-robot/src/main/cpp/robot.cpp
+++ frc-2019-robot/src/main/cpp/robot.cpp
@@ -1,54 +1,37 @@
 #include <frc/Joystick.h>
-#include <ctre/Phoenix.h>
-#include <frc/IterativeRobot.h>
-#include <unistd.h> 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
-#include <string.h> 
+#include <frc/TimedRobot.h>
+#include <ctre/phoenix/motorcontrol/ControlMode.h>
 
-class Robot : public frc::IterativeRobot {
-public:
-    void TeleopInit() override {
-        m_LeftSPX.Follow(m_LeftSRX);
-        m_RightSPX.Follow(m_RightSRX);
-        m_LeftSPX.SetInverted(true);
-        int serverDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
-        if (serverDescriptor == 0) {
+#include "ctre.hpp"
 
-        } else {  
-            int opt;
-            if (setsockopt(serverDescriptor, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-                struct sockaddr_in address {
-                  AF_INET,
-                  htons(5800),
-                  { INADDR_ANY }
-                };
-            } else {
-
-            }
+namespace rbt {
+    class Robot : public frc::TimedRobot {
+    public:
+        void TeleopInit() override {
+            m_LeftSPX.Follow(m_LeftSRX);
+            m_RightSPX.Follow(m_RightSRX);
+            m_LeftSPX.SetInverted(true);
         }
-    }
 
-    void TeleopPeriodic() override {
-        double
-                forward = -m_Stick.GetY(),
-                turn = m_Stick.GetX();
-        m_LeftSRX.Set(ControlMode::PercentOutput, forward + turn * 2);
-        m_RightSRX.Set(ControlMode::PercentOutput, forward - turn * 2);
-    }
+        void TeleopPeriodic() override {
+            double
+                    forward = -m_Stick.GetY(),
+                    turn = m_Stick.GetX();
+            m_LeftSRX.Set(ctr::ControlMode::PercentOutput, forward + turn * 2);
+            m_RightSRX.Set(ctr::ControlMode::PercentOutput, forward - turn * 2);
+        }
 
-private:
-    frc::Joystick m_Stick{0};
-    TalonSRX m_RightSRX{0}, m_LeftSRX{1};
-    VictorSPX m_RightSPX{2}, m_LeftSPX{3};
-};
+    private:
+        frc::Joystick m_Stick{0};
+        ctr::TalonSRX m_RightSRX{0}, m_LeftSRX{1};
+        ctr::VictorSPX m_RightSPX{2}, m_LeftSPX{3};
+    };
+}
 
 #ifndef RUNNING_FRC_TESTS
 
 int main() {
-    return frc::StartRobot<Robot>();
+    return frc::StartRobot<rbt::Robot>();
 }
 
 #endif

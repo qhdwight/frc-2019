a6b6dd80599ce8d412c41e7a16f9049e6291c092
BuildTools <qhdwight@gmail.com>
2019-02-24 15:32:35 -0800

    Updated logger format to use templating

--- frc-2019-robot/src/main/include/lib/logger.hpp
+++ frc-2019-robot/src/main/include/lib/logger.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <memory>
 
 namespace garage {
     namespace lib {
@@ -12,11 +13,17 @@ namespace garage {
         private:
             LogLevel m_LogLevel;
         public:
-            std::string Format(std::string format, ...);
+            template<typename ... Args>
+            std::string Format(const std::string& format, Args ... args) {
+                auto size = static_cast<size_t>(std::snprintf(nullptr, 0, format.c_str(), args ...) + 1);
+                std::unique_ptr<char[]> buffer(new char[size]);
+                std::snprintf(buffer.get(), size, format.c_str(), args ...);
+                return std::string(buffer.get(), buffer.get() + size - 1);
+            }
 
             void SetLogLevel(LogLevel logLevel);
 
-            void Log(LogLevel logLevel, std::string log);
+            void Log(LogLevel logLevel, const std::string& log);
         };
     }
 }
--- frc-2019-robot/src/main/include/lib/subsystem_controller.hpp
+++ frc-2019-robot/src/main/include/lib/subsystem_controller.hpp
@@ -24,7 +24,7 @@ namespace garage {
 
             void Log(LogLevel logLevel, const std::string& log) {
                 auto subsystem = std::dynamic_pointer_cast<Subsystem>(m_Subsystem);
-                subsystem->Log(logLevel, subsystem->GetLogger()->Format(" [%s] %s", m_Name.c_str(), log.c_str()));
+                subsystem->Log(logLevel, subsystem->GetLogger()->Format(" [%s] %s", m_Name, log));
             }
 
             virtual void OnEnable() {
